0f28d0955304bc2fd1b2d4c56a6f6d85708e9677
Kevin Griffin <kg@scoir.com>
2020-04-29 22:30:55 -0400

    ignoring compiled typescript, it's a long and confusing story but we're not really _using_ it, merely for typing props

--- dist/tsc/navigation/app-routes.js
+++
@@ -1,8 +0,0 @@
-export var AppRoute;
-(function (AppRoute) {
-    AppRoute["HOME"] = "Home";
-    AppRoute["CONNECT"] = "Connect";
-    AppRoute["CONNECTIONS"] = "Connections";
-    AppRoute["CREDENTIALS"] = "Credentials";
-})(AppRoute || (AppRoute = {}));
-//# sourceMappingURL=app-routes.js.map
--- dist/tsc/navigation/app.navigator.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { HomeNavigator } from './home.navigator';
-import { AppRoute } from './app-routes';
-const Stack = createStackNavigator();
-export const AppNavigator = (props) => (React.createElement(Stack.Navigator, Object.assign({}, props, { headerMode: 'none' }),
-    React.createElement(Stack.Screen, { name: AppRoute.HOME, component: HomeNavigator })));
-//# sourceMappingURL=app.navigator.js.map
