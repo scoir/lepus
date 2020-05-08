cfc553c979638ea41975a31312e75b6733dec56d
Kevin Griffin <kg@scoir.com>
2020-05-07 22:10:02 -0400

    added lfs while trying to get rid of the need for lfs....
    Former-commit-id: 74c4c56255eb39e0954f9e86597dc07a39cd855e

--- scripts/ios.sh
+++ scripts/ios.sh
@@ -4,6 +4,6 @@ ESQUIVE_ROOT=$(pwd)
 HOST_IP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 
 echo -e "🛠  Building..."
-cd go/nymble && gomobile bind -o "$ESQUIVE_ROOT"/ios/esquive/nymble.framework -target=ios -tags=debug -ldflags "-X log.HostIP=$HOST_IP" > /dev/null
+cd go/nymble && gomobile bind -o "$ESQUIVE_ROOT"/ios/esquive/nymble.framework -target=ios > /dev/null
 
 echo -e "✨ xcode should pick up the new framework automagically"
