6377a4b5a22a7e7bcf972e80109a98a917b1d1a6
griff <griffin.kev@gmail.com>
2020-05-28 11:23:15 -0400

    oops

--- scripts/android.sh
+++ scripts/android.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+# https://unix.stackexchange.com/questions/175648/use-config-file-for-my-shell-script
+source ./scripts/config.shlib;
 
 ESQUIVE_ROOT=$(pwd)
+
+ROUTER_ADDRESS="-X pkg/nymble/config.RouterAddress=$(config_get host_ip)"
+ROUTER_PORT="-X pkg/nymble/config.RouterPort=$(config_get router_port)"
+DEBUG_IP="-X pkg/nymble/config.DebugIP=$(config_get host_ip)"
+DEBUG_PORT="-X pkg/nymble/config.DebugPort=$(config_get debug_port)"
+
+LDFLAGS="${ROUTER_ADDRESS} ${ROUTER_PORT} ${DEBUG_IP} ${DEBUG_PORT}"
 
 echo -e "🛠  Building..."
 cd go/nymble && gomobile \
